982bfe72772eccd33c664c7f774789a047b1bf96
barbosaadriano <faculdadealfa@Mac-19.local>
2016-11-19 15:31:48 -0200

    LOcations

--- AlcoolGasolina/MapaViewController.m
+++ AlcoolGasolina/MapaViewController.m
@@ -40,5 +40,41 @@
     MKCoordinateRegion region = MKCoordinateRegionMake(pino.coordinate, zoom);
     [mapa setRegion:region animated:YES];
 }
-
+-(IBAction)mudarTipo:(id)sender
+{
+    switch (mapa.mapType) {
+        case MKMapTypeStandard:
+            [mapa setMapType:MKMapTypeSatellite];
+            break;
+        case MKMapTypeSatellite:
+            [mapa setMapType:MKMapTypeHybrid];
+            break;
+        default:
+            [mapa setMapType:MKMapTypeStandard];
+            break;
+    }
+}
+-(IBAction)ondeEstou:(id)sender {
+    [self limparPinos];
+    if ([CLLocationManager locationServicesEnabled]){
+        if (!locaManager) {
+            locaManager = [[CLLocationManager alloc] init];
+            locaManager.delegate = self;
+        }
+        [locaManager startUpdatingLocation];
+    }
+}
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *myLocation = [locations lastObject];
+    MKPointAnnotation *pino = [[MKPointAnnotation alloc] init];
+    pino.coordinate = myLocation.coordinate;
+    pino.title = @"Estou aqui!!";
+    pino.subtitle = [NSString stringWithFormat:@"Lat: %f, Long: %f, Alt: %f", myLocation.coordinate.latitude,myLocation.coordinate.longitude,myLocation.altitude];
+    [mapa addAnnotation:pino];
+    MKCoordinateSpan zoom = MKCoordinateSpanMake(0.01, 0.01);
+    MKCoordinateRegion region = MKCoordinateRegionMake(pino.coordinate, zoom);
+    [mapa setRegion:region animated:YES];
+    [locaManager stopUpdatingLocation];
+}
 @end
